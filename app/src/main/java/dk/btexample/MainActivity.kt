fb9b45462bb2b3e55e9568cdf1187937485696e2
Denis Kokarev <d_kokarev@mail.ru>
2022-05-15 15:41:39 -0400

    add BLE Scan, alas not working

--- app/src/main/java/dk/btexample/MainActivity.kt
+++ app/src/main/java/dk/btexample/MainActivity.kt
@@ -1,7 +1,6 @@
 package dk.btexample
 
-import android.Manifest.permission.BLUETOOTH
-import android.Manifest.permission.BLUETOOTH_ADMIN
+import android.Manifest.permission.*
 import android.app.Activity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -9,15 +8,20 @@ import dk.btexample.databinding.ActivityMainBinding
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.util.Log
 import android.view.View
+import androidx.annotation.RequiresPermission
+import com.st.BlueSTSDK.Manager
+import com.st.BlueSTSDK.Node
+import com.st.BlueSTSDK.Utils.advertise.AdvertiseFilter
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), Manager.ManagerListener {
     private val TAG = this::class.simpleName
     private val REQUEST_BT_PERMISSIONS = 1
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var scanner: Scanner
 
-    private val permissions = arrayOf(BLUETOOTH, BLUETOOTH_ADMIN)
+    private val permissions = arrayOf(ACCESS_FINE_LOCATION, BLUETOOTH, BLUETOOTH_ADMIN)
+
+    lateinit var mManager: Manager
 
     private fun checkScanPermission() = permissions.all {
         ActivityCompat.checkSelfPermission(
@@ -30,12 +34,17 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "BT onCreate()")
 
-        scanner = Scanner(applicationContext)
+        mManager = Manager.getSharedInstance()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    fun onScanButton(view: View) {
+    override fun onPause() {
+        super.onPause()
+        stopDiscovery()
+    }
+
+    fun onStartScanButton(view: View) {
         if (!checkScanPermission()) {
             ActivityCompat.requestPermissions(
                 this,
@@ -44,8 +53,31 @@ class MainActivity : Activity() {
             )
             Log.d(TAG, "BT Permissions not granted")
         } else {
-            scanner.scanLeDevices()
+            startDiscovery()
         }
+    }
+
+    fun onStopScanButton(view: View) {
+        stopDiscovery()
+    }
+
+    /**
+     * build a list of object used to filter the node to display in the activity
+     * @return a filter to show the node with the advertise format defined by the BlueSTSDK specs
+     */
+    protected fun buildAdvertiseFilter(): List<AdvertiseFilter?>? {
+        return Manager.buildDefaultAdvertiseList()
+    }
+
+    @RequiresPermission(allOf = [ACCESS_FINE_LOCATION, BLUETOOTH, BLUETOOTH_ADMIN])
+    private fun startDiscovery() {
+        mManager.addListener(this)
+        mManager.startDiscovery(10_000, buildAdvertiseFilter())
+    }
+
+    private fun stopDiscovery() {
+        mManager.removeListener(this)
+        mManager.stopDiscovery()
     }
 
     override fun onRequestPermissionsResult(
@@ -56,11 +88,19 @@ class MainActivity : Activity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_BT_PERMISSIONS) {
             if (checkScanPermission()) {
-                scanner.scanLeDevices()
+                startDiscovery()
             } else {
                 Log.e(TAG, "User doesn't allow scanning BLE devices")
             }
         }
+    }
+
+    override fun onDiscoveryChange(m: Manager, enabled: Boolean) {
+        Log.i(TAG, "discovery changed = $m enabled = $enabled")
+    }
+
+    override fun onNodeDiscovered(m: Manager, node: Node) {
+        Log.i(TAG, "node = $node")
     }
 
 }
